8ff0800c7cfa506c715c0fc1e2cba168ab548029
aashishgavit <aashishgavit29@gmail.com>
2026-08-26 14:54:57 +0530

    feat: add test controller for CI/CD Docker pipeline

---
+++ src/main/java/com/Deploy/TestController.java
@@ -0,0 +1,19 @@
+package com.Deploy;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController
+{
+    @GetMapping("/test")
+    public ResponseEntity testing()
+    {
+        return new ResponseEntity("Testing Docker/CICD", HttpStatus.OK);
+    }
+}
