5ae4130abde90dac7deb7478bf4a9d9007f607f7
aashishgavit <aashishgavit29@gmail.com>
2026-08-26 09:54:26 +0530

    Initial commit

---
+++ src/main/java/com/Deploy/DeployHubApplication.java
@@ -0,0 +1,13 @@
+package com.Deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployHubApplication.class, args);
+	}
+
+}
